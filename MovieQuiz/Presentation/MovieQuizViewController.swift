9e1937939fcab7351f1d65d33b4278f1303b5f06
Aelybs <aminaturarbekova22@gmail.com>
2025-10-06 10:48:20 +0300

    Установка слабых ссылок

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -82,7 +82,8 @@ final class MovieQuizViewController: UIViewController {
             message: "Ваш результат \(correctAnswer)/10",
             preferredStyle: .alert)
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
             self.currentQuestionIndex = 0
             self.correctAnswer = 0
             
@@ -130,7 +131,8 @@ final class MovieQuizViewController: UIViewController {
         UIColor.ypRed.cgColor
         
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.showNextQuestionOrResults()
         }
     }
